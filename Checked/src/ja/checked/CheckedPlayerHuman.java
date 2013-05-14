51cb8b3c3b7e61577af0b7a3112aa736b8ead8e9
Jeff Adams <jeff@jamodeler.com>
2013-05-14 16:44:57 -0400

    Added CheckedPlayerHuman.java and updated .gitignore

---
+++ Checked/src/ja/checked/CheckedPlayerHuman.java
@@ -0,0 +1,40 @@
+package ja.checked;
+
+import java.awt.Color;
+
+public class CheckedPlayerHuman extends CheckedPlayer {
+	
+	private CheckedMove choosenMove;
+	
+	CheckedPlayerHuman()
+	{
+		
+	}
+	
+	CheckedPlayerHuman(Color color, int playerNum)
+	{
+		super(color,playerNum);
+		setAI(false);
+		setChoosenMove(null);
+	}
+
+	public CheckedMove getChoosenMove() 
+	{
+		
+		return choosenMove;
+	}
+
+	public void setChoosenMove(CheckedMove choosenMove) 
+	{
+		this.choosenMove = choosenMove;
+	}
+	
+	@Override
+	public CheckedMove getMove()
+	{
+		CheckedMove returnMove = choosenMove;
+		choosenMove = null;  //clear the choosenmove 
+		return returnMove;
+	}
+
+}
